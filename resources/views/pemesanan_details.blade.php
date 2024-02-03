dae1f5aff78faff7733aac33954a7415f6f08952
andreasprass <andreasprasetya29@gmail.com>
2024-02-03 12:11:25 +0700

    tambah modal

--- resources/views/pemesanan_details.blade.php
+++ resources/views/pemesanan_details.blade.php
@@ -156,19 +156,44 @@
                           @csrf
                           @method('put')
                           <button class="btn btn-sm btn-success" onclick="return confirm('Are you sure?')"><span>Telah Dikembalikan</span></button>
-                      </form>
+                        </form>
+                        <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#input_km_akhir">Dikembalikan</button>
                       </div>
                     </div>
                   </div>
                 </div>
                 <!-- /.card-body -->
               </div>
+
+              <!-- Modal -->
+              <div class="modal fade " id="input_km_akhir" tabindex="-1" aria-labelledby="input_km_akhir" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form action="">
+                        <input type="text" placeholder="Masukan KM Akhir">
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
         </div>
     </div>
         <!-- /.row -->
       <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+
 
   
 @endsection
